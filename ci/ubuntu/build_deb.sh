0eb499e6a84482a22275be6deee272f63593e0a3
Federico Terzi <federicoterzi96@gmail.com>
2020-08-02 11:04:27 +0200

    Improve CI

--- ci/ubuntu/build_deb.sh
+++ ci/ubuntu/build_deb.sh
@@ -4,17 +4,19 @@ echo "Testing modulo..."
 cd modulo
 cargo test --release
 
+ls
+
 echo "Building modulo and packaging deb"
 cargo deb -p modulo
 
 cd ..
 cp modulo/target/debian/modulo*.deb /shared/modulo-debian-amd64.deb
 
-echo "Copying to mounted volume"
-cp modulo-debian-* /shared
-
 echo "Building binary"
+pushd modulo
 cargo build --release
+popd
+
 cp modulo/target/release/modulo /shared/modulo-linux-amd64
 
 ls /shared
